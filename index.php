a5b413367ddd20bd4f725c350b5b44b421938852
ahmad sirojuddin kamil <ahmadsirojuddinkamil@gmail.com>
2023-02-09 20:04:59 +0700

    php if else

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $teman = "andi";
+    if ($teman == "andi") {
+        echo "dia adalah teman saya";
+    } else {
+        echo "dia bukan teman saya";
+    }
+    ?>
+</body>
+
+</html>
